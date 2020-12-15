ecc9710f2579bd636e6c36a223cd340a7533e950
Mariusz Kok <kokmar76@gmail.com>
2020-12-15 20:10:04 +0100

    18.4 Tworzenie ontrolera - zadanie: mapowanie metod Controllera

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDTO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public List<TaskDTO> getTasks() {
+        return new ArrayList<>();
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    public TaskDTO getTask(Long taskId) {
+        return new TaskDTO(1L, "test title", "test_content");
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+    public void deleteTask(Long taskId) {
+
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDTO updateTask(TaskDTO taskDto) {
+        return new TaskDTO(1L, "Edited test title", "Test content");
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public void createTask(TaskDTO taskDto) {
+
+    }
+}
