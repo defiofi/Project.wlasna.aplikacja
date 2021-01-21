1515c416a21b2c8929787cd8417a058b35494842
Mariusz Kok <kokmar76@gmail.com>
2021-01-21 21:06:25 +0100

    22.2 Rest Template - Zadanie: ułożenie kodu

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,28 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
+@RequiredArgsConstructor
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+        trelloBoards.stream()
+                    .filter(n->n.getName().contains("Kodilla"))
+                    .filter(n->n.getId().length()>0)
+                    .forEach(s -> System.out.println(s.getId()+ " "+s.getName()));
+    }
+}
